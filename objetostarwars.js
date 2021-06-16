bd60795358f372a497dcf88ef2987cee8bd241c7
arthurbbarros <arthurbaraujo92@gmail.com>
2021-06-16 12:37:22 -0300

    lista de objetos/filmes

---
+++ objetostarwars.js
@@ -0,0 +1,91 @@
+// Lista de ojbetos 
+var f1= {
+    nome:"Uma Nova Esperança", 
+    ano: 1977,
+    ordenaçao:1,
+    imagem:<img src="https://www.cine14bis.com.br/site/wp-content/uploads/2019/10/20172772.jpg-r_1920_1080-f_jpg-q_x-xxyxx.jpg"></img>, 
+    secao:"Trilogia Clássica"
+}
+
+
+var f2={
+    nome:"O Império Contra Ataca",
+    ano: 1980,
+    ordenaçao:2,
+    imagem:<img src="https://br.web.img3.acsta.net/medias/nmedia/18/72/73/89/20535463.jpg"></img>,
+    secao:"Trilogia Clássica"
+}
+
+var f3={
+    nome:"O retorno Jedi",
+    ano: 1983,
+    ordenaçao:3,
+    imagem:<img src="https://br.web.img3.acsta.net/c_310_420/pictures/14/05/19/22/57/276761.jpg"></img>,
+    secao:"Trilogia Clássica"
+}
+
+var f4={
+    nome:"A Ameaça Fantasma",
+    ano: 1993,
+    ordenaçao:4,
+    imagem:<img src="https://upload.wikimedia.org/wikipedia/pt/thumb/3/30/Star_Wars_Epis%C3%B3dio_1_Amea%C3%A7a_Fantasma.jpg/240px-Star_Wars_Epis%C3%B3dio_1_Amea%C3%A7a_Fantasma.jpg"></img>,
+    secao:"Segunda Trilogia/Prequels"
+}
+
+var f5={
+    nome:"Ataque dos Clones",
+    ano:2002, 
+    ordenaçao:5,
+    imagem:<img src="https://upload.wikimedia.org/wikipedia/pt/thumb/6/63/Star_Wars_The_Clone_Wars.jpg/250px-Star_Wars_The_Clone_Wars.jpg"></img>,
+    secao:"Segunda Trilogia/Prequels"
+}
+
+var f6={
+    nome:"A Vingança dos Sith",
+    ano:2005, 
+    ordenaçao:6,
+    imagem:<img src="https://upload.wikimedia.org/wikipedia/pt/5/58/Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg"></img>,
+    secao:"Segunda Trilogia/Prequels"
+}
+
+var f6={
+    nome:"O Despertar da Força",
+    ano:2015, 
+    ordenaçao:7,
+    imagem:<img src="https://images-na.ssl-images-amazon.com/images/I/81QWAGT3FdL._AC_SY550_.jpg"></img>,
+    secao:"Terceira Trilogia"
+}
+
+var f7={
+    nome:"Rogue one: Uma História Star Wars",
+    ano: 2016,
+    ordenaçao:8,
+    imagem:<img src="https://images-na.ssl-images-amazon.com/images/I/71CcCQcddsL.jpg"></img>,
+    secao:"Spinn-offs"
+}
+
+var f8={
+    nome:"Os últimos jedis",
+    ano: 2017,
+    ordenaçao:9,
+    imagem:<img src="https://cdn.fstatic.com/media/movies/covers/2017/10/Sem_t%C3%ADtulo.png"></img>,
+    secao:"Terceira Trilogia"
+}
+
+var f9={
+    nome:"Han Solo: Uma História Star Wars",
+    ano:2018, 
+    ordenaçao:10
+    imagem:<img src="https://poltronanerd.com.br/wp-content/uploads/2018/04/poltrona-han-solo-poster-24abr18-3.jpg"></img>,
+    secao:"Spinn-offs"
+}
+
+var f10={
+    nome:"A Ascenção de Skywalker",
+    ano: 2019
+    ordenaçao:11
+    imagem:<img src="https://blogdojotace.com.br/wp-content/uploads/2020/02/sw9-bd2.jpg"></img>,
+    secao:"Terceira Trilogia"
+}
+
+
