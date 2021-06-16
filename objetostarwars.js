dc47349254d8d2920173164a8e16415f4ba2f171
Gildo Neto <gildorama@gmail.com>
2021-06-16 19:41:07 -0300

    adiciona secao

--- objetostarwars.js
+++ objetostarwars.js
@@ -4,6 +4,7 @@ var filme1 = {
     ano: 1977,
     ordenacao: 1, 
     episodio: "IV",
+    secao: "classica",
     imagem: "https://www.cine14bis.com.br/site/wp-content/uploads/2019/10/20172772.jpg-r_1920_1080-f_jpg-q_x-xxyxx.jpg"    
 }
 var filme2 = {
@@ -11,6 +12,7 @@ var filme2 = {
     ano: 1980,
     ordenaçao: 2,
     episodio: "V",
+    secao: "classica",
     imagem: "https://br.web.img3.acsta.net/medias/nmedia/18/72/73/89/20535463.jpg"
 }
 var filme3 = {
@@ -18,6 +20,7 @@ var filme3 = {
     ano: 1983,
     ordenaçao: 3,
     episodio: "VI",
+    secao: "classica",
     imagem: "https://br.web.img3.acsta.net/c_310_420/pictures/14/05/19/22/57/276761.jpg"
 }
 var filme4 = {
@@ -25,6 +28,7 @@ var filme4 = {
     ano: 1993,
     ordenaçao: 4,
     episodio:"I",
+    secao: "prequel",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/3/30/Star_Wars_Epis%C3%B3dio_1_Amea%C3%A7a_Fantasma.jpg/240px-Star_Wars_Epis%C3%B3dio_1_Amea%C3%A7a_Fantasma.jpg"
 }
 var filme5 = {
@@ -32,6 +36,7 @@ var filme5 = {
     ano: 2002, 
     ordenaçao: 5,
     episodio: "II",
+    secao: "prequel",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/6/63/Star_Wars_The_Clone_Wars.jpg/250px-Star_Wars_The_Clone_Wars.jpg" 
 }
 var filme6 = {
@@ -39,24 +44,28 @@ var filme6 = {
     ano: 2005, 
     ordenaçao: 6,
     episodio: "III",
+    secao: "prequel",
     imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/5/58/Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg/250px-Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg"
 }
 var filme7 = {
     nome: "Rogue One",
     ano: 2016, 
     ordenaçao: 7,
+    secao: "spinoff",
     imagem: "https://images-na.ssl-images-amazon.com/images/I/71CcCQcddsL.jpg"
 }
 var filme8 = {
     nome: "Han Solo",
     ano: 2018,
     ordenaçao: 8,
+    secao: "spinoff",
     imagem: "https://poltronanerd.com.br/wp-content/uploads/2018/04/poltrona-han-solo-poster-24abr18-3.jpg"
 }
 var filme9 = {
     nome: "The Mandalorian",
     ano: 2019,
     ordenaçao: 9,
+    secao: "spinoff",
     imagem: 'https://cdn.europosters.eu/image/750/posters/star-wars-the-mandalorian-dusk-i81805.jpg'
 }
 var filme10 = {
@@ -64,6 +73,7 @@ var filme10 = {
     ano: 2015, 
     ordenaçao: 10,
     episodio: "VII",
+    secao: "nova",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/a/ae/Starwars_06.jpg/250px-Starwars_06.jpg"
 }
 var filme11 = {
@@ -71,6 +81,7 @@ var filme11 = {
     ano: 2017,
     ordenaçao: 11,
     episodio: "VIII",
+    secao: "nova",
     imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/0/0e/Star_Wars_The_Last_Jedi.png/250px-Star_Wars_The_Last_Jedi.png"
 }
 var filme12 = {
@@ -78,6 +89,7 @@ var filme12 = {
     ano: 2019,
     ordenacao: 12,
     episodio: "IX",
+    secao: "nova",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/0/08/Star_Wars_The_Rise_of_Skywalker.jpg/250px-Star_Wars_The_Rise_of_Skywalker.jpg"
 }
 
